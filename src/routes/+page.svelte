b2083ae762328fa9e059d648fb8440358b1c819a
Crystal Bajgai <crystalbajgai@gmail.com>
2025-02-24 20:45:30 +0545

    Colors and font import.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-heading italic">Welcome to SvelteKit</h1>
+<p class="text-paragraph">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button class="text-highlight">Click me</button>
