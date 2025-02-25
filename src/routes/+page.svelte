64dd369d773a0a53bd9edd7b40345e38ef4f5e78
Crystal Bajgai <crystalbajgai@gmail.com>
2025-02-25 10:04:39 +0545

    Added main section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,30 @@
-<h1 class="text-heading italic">Welcome to SvelteKit</h1>
-<p class="text-paragraph">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<button class="text-highlight">Click me</button>
+<div class="flex min-h-screen items-center justify-center">
+	<section id="main">
+		<h1 class="text-heading text-center text-2xl font-bold md:text-6xl mx-2 2xl:text-8xl">
+			Hi, I am Crystal Bajgai.
+		</h1>
+		<p class="text-paragraph mt-4 2xl:mt-6 text-center text-xl font-medium md:text-4xl mx-2 2xl:text-6xl">
+			I love building cool things and trying out new ideas.
+		</p>
+		<div class="mt-6 flex flex-row justify-center space-x-8 md:space-x-10 2xl:mt-8 2xl:space-x-12">
+			<!--github-->
+			<a
+				href="https://github.com/crystalbajgai"
+				aria-label="github icon"
+				target="_blank"
+				class=""
+			>
+				<i class="fa-brands fa-github text-4xl md:text-6xl text-paragraph hover:text-heading"></i>
+			</a>
+			<!--linkedin-->
+			<a
+				href="https://www.linkedin.com/in/crystalbajgai/"
+				aria-label="linkedin icon"
+				target="_blank"
+				class=""
+			>
+				<i class="fa-brands fa-linkedin text-4xl md:text-6xl text-paragraph hover:text-heading"></i>
+			</a>
+		</div>
+	</section>
+</div>
