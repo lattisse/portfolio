26ebb0979f5b0f939e743dfd13ff02ff92884053
Crystal Bajgai <crystalbajgai@gmail.com>
2025-02-25 19:09:37 +0545

    Projects page boilerplate

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,8 @@
+<div class="flex min-h-dvh">
+	<div class="w-full flex flex-col mt-8">
+		<h1 class="text-heading text-2xl font-medium md:text-4xl  text-center">Blog</h1>
+		<div class="flex-grow flex items-center justify-center mb-8">
+			<p class="text-paragraph text-xl md:text-2xl transform">Uh oh, it seems empty right now.</p>
+		</div>
+	</div>
+</div>
